f7baf200e483bf5a39d619cec674174f5b4f2399
Leigh Williams <leigh.w.mail@gmail.com>
2013-12-10 12:42:26 +1100

    Add remaining JSON models

---
+++ app/assets/javascripts/menu_click_events.js
@@ -0,0 +1,61 @@
+// == menu click events ==
+
+// Display Modes
+$("#BS").click(function() {
+  $(this).addClass("success");
+  $("#SF").removeClass("success");
+  $("#WF").removeClass("success");
+  viewer.specs.set3DRepresentation('Ball and Stick');
+  viewer.updateScene()
+});
+
+$("#SF").click(function() {
+  $(this).addClass("success");
+  $("#BS").removeClass("success");
+  $("#WF").removeClass("success");
+  viewer.specs.set3DRepresentation('van der Waals Spheres');
+  viewer.updateScene()
+});
+
+$("#WF").click(function() {
+  $(this).addClass("success");
+  $("#BS").removeClass("success");
+  $("#SF").removeClass("success");
+  viewer.specs.set3DRepresentation('Wireframe');
+  viewer.updateScene()
+});
+
+// Labels
+$("#Labs").click(function() {
+  viewer.specs.atoms_displayLabels_3D =! viewer.specs.atoms_displayLabels_3D;
+  viewer.updateScene()
+});
+
+// Load models
+$("#PS").change(function() {
+  var model = $("#PS").val();
+
+  switch(model) {
+    case "4F5S":
+      molFile = pdb_4F5S;
+      viewer.loadMolecule(molFile);
+      break;
+    case "1BEB":
+      molFile = pdb_1BEB;
+      viewer.loadMolecule(molFile);
+      break;
+    case "1BLF":
+      molFile = pdb_1BLF;
+      viewer.loadMolecule(molFile);
+      break;
+    case "1B8E":
+      molFile = pdb_1B8E;
+      viewer.loadMolecule(molFile);
+      break;
+    case "1F6F":
+      molFile = pdb_1F6F;
+      viewer.loadMolecule(molFile);
+      break;
+  }
+
+});
