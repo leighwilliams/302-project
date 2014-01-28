27403db0fe289fe09d6d22925b36803d5cb6121a
Leigh Williams <leigh.w.mail@gmail.com>
2014-01-29 00:40:07 +1100

    Add touch device controls in footer.

--- app/assets/javascripts/custom_chemdoodle.js
+++ app/assets/javascripts/custom_chemdoodle.js
@@ -1,22 +1,19 @@
 // Custom ChemDoodle Web Components functions.
-//$(function() {
-  // the module
-  // (function(ChemDoodle) {
+// the module
+// (function(ChemDoodle) {
 
-  //   // private variables go here, they cannot be accessed beyond this local scope
-  //   var labelToMatch = 'N';
+//   // private variables go here, they cannot be accessed beyond this local scope
+//   var labelToMatch = 'N';
 
-  //   // append the function to the ChemDoodle variable
-  //   ChemDoodle.countNitrogens = function(molecule){
-  //     var count = 0;
-  //     for(var i = 0, ii = molecule.atoms.length; i<ii; i++){
-  //       if(molecule.atoms[i].label==labelToMatch){
-  //         count++;
-  //       }
-  //     }
-  //     return count;
-  //   };
+//   // append the function to the ChemDoodle variable
+//   ChemDoodle.countNitrogens = function(molecule){
+//     var count = 0;
+//     for(var i = 0, ii = molecule.atoms.length; i<ii; i++){
+//       if(molecule.atoms[i].label==labelToMatch){
+//         count++;
+//       }
+//     }
+//     return count;
+//   };
 
-  // })(ChemDoodle);
-
-//}); // jQuery ready.
+// })(ChemDoodle);
